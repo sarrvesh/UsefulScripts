70380d766102a306273cd2f9c8f7c2c6ee07a095
sarrvesh <sarrvesh.ss@gmail.com>
2015-06-08 19:21:39 +0200

    merging duplicate code

--- makeFitsCube.py
+++ makeFitsCube.py
@@ -17,10 +17,6 @@ try:
 except ImportError:
     raise Exception('Unable to import Numpy')
 try:
-    import psutil
-except ImportError:
-    raise Exception('Unable to import psutil.')
-try:
     import pyfits as pf
 except ImportError:
     raise Exception('Unable to import pyFits.')
@@ -48,27 +44,9 @@ def checkFitsShape(fitsList):
     for i, name in enumerate(fitsList):
         if i == 0:
             templateShape = pf.open(name, readonly=True)[0].data[0].shape
-            templateSize = pf.open(name, readonly=True)[0].data[0].nbytes
-        elif templateShape == pf.open(name, readonly=True)[0].data[0].shape:
-            pass
-        else:
+        elif templateShape != pf.open(name, readonly=True)[0].data[0].shape:
             raise Exception('Fits file {} has an incompatible shape'.format(name))
-    return templateShape, templateSize
-
-def concatenateWithRAM(validFitsList, shape):
-    """
-    Concatenate a given list of fits files into a single cube using RAM
-    """
-    concatCube = np.zeros((1, len(validFitsList), shape[-2], shape[-1]))
-    for i, name in enumerate(validFitsList):
-        tempData = pf.open(name, readonly=True)[0].data[0]
-        if len(shape) == 2:
-            concatCube[0, i, :] = tempData[:]
-        if len(shape) == 3:
-            concatCube[0, i, :] = tempData[0, :]
-        if len(shape) == 4:
-            concatCube[0, i, :] = tempData[0, 0, :]
-    return concatCube
+    return templateShape
 
 def concatenateWithMemMap(validFitsList, shape, memMapName):
     """
@@ -76,10 +54,10 @@ def concatenateWithMemMap(validFitsList, shape, memMapName):
     """
     concatCube = np.memmap(memMapName, dtype='float32', mode='w+',\
                            shape=(1, len(validFitsList), shape[-2], shape[-1]))
+    freqList = []
     for i, name in enumerate(validFitsList):
         tempData = pf.open(name, readonly=True)[0].data[0]
-        if len(shape) == 2:
-            concatCube[0, i, :] = tempData[:]
+        tempHead = pf.open(name, readonly=True)[0].header
         if len(shape) == 3:
             concatCube[0, i, :] = tempData[0, :]
         if len(shape) == 4:
@@ -91,36 +69,31 @@ def main(options):
     Main function
     """
     memMapName = 'memMap'
+
     # Check user input
     if options.inp == '':
         raise Exception('An input glob string must be specified.')
     if options.out == '':
         raise Exception('An output filename must be specified.')
+
     # Get the list of FITS files
     fileList = sorted(glob.glob(options.inp))
     validFitsList = getValidFitsList(fileList)
     print 'INFO: Identified {} fits files from {} files selected by input string'.\
           format(len(validFitsList), len(fileList))
+
     # Proceed with the execution if we have non-zero FITS files
     if len(validFitsList) == 0:
         raise Exception('No valid fits files were selected by the glob string')
+
     # Check if the list of supplied fits files have the same shape
-    shape, fitsSize = checkFitsShape(validFitsList)
+    shape = checkFitsShape(validFitsList)
     print 'INFO: All fits files have shape {}'.format(shape)
-    if len(shape) not in [2, 3, 4]:
+    if len(shape) not in [3, 4]:
         raise Exception('Fits files have unknown shape')
-    totalArraySize = fitsSize*len(validFitsList)/(1024*1024)
-    print 'INFO: Total required memory is {} MB'.format(totalArraySize)
-    availMem = psutil.virtual_memory().available/(1024*1024)
-    print 'INFO: Available physical memory is {} MB'.format(availMem)
-    # Decide whether to use RAM or memory map to store the temporary files
-    if totalArraySize > availMem:
-        print 'INFO: Memory required by the code is greater than available memory.'
-        print 'INFO: Will use memory map to store temporary files.'
-        finalCube = concatenateWithMemMap(validFitsList, shape, memMapName)
-    else:
-        print 'INFO: Using physical memory to store temporary files.'
-        finalCube = concatenateWithRAM(validFitsList, shape)
+
+    finalCube = concatenateWithMemMap(validFitsList, shape, memMapName)
+
     # Get a template header from a fits file
     header = pf.open(validFitsList[0], readonly=True)[0].header
     print 'INFO: Writing the concatenated fits file to {}'.format(options.out)
@@ -135,7 +108,7 @@ if __name__ == '__main__':
     opt.add_option('-o', '--out', help='Filename of the output cube '+
                    '[default: mergedFits.fits]', default='mergedFits.fits')
     opt.add_option('-f', '--freq',
-                   help='Write the list of frequencies to a text file [default: False]',
-                   default=False, action='store_true')
+                   help='Filename to write the frequency list [default: frequency.txt]',
+                   default='frequency.txt')
     inOpts, arguments = opt.parse_args()
     main(inOpts)
